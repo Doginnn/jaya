a09ecca3436058567d75dda5579fc17e98a64fe1
Doginnn <diogenesemmanuel@gmail.com>
2024-02-22 18:15:52 -0300

    [UPD] Incrementing fillables in the model.

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $table = 'payments';
+
+    protected $fillable = [
+        'transaction_amount',
+        'installments',
+        'token',
+        'payment_method_id',
+        'payer_entity_type',
+        'payer_type',
+        'payer_email',
+        'payer_identification_type',
+        'payer_identification_number',
+        'notification_url',
+        'created_at',
+        'updated_at',
+        'status'
+    ];
 }
