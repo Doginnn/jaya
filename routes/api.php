ae2f31327a0ca8f85b8fa36095056f70b12c007b
Doginnn <diogenesemmanuel@gmail.com>
2024-02-23 20:40:43 -0300

    [UPD] Defining the routes from API.

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,9 @@ use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 # Routes to Payments
-Route::post('/payments', [PaymentController::class, 'store']);
+//Route::get('/rest/payments', [PaymentController::class, 'index']);
+Route::post('/rest/payments', [PaymentController::class, 'store']);
+//Route::get('/rest/payments', [PaymentController::class, 'index']);
+//Route::get('/rest/payments/{id}', [PaymentController::class, 'show']);
+//Route::put('/rest/payments/{id}', [PaymentController::class, 'update']);
+//Route::delete('/rest/payments/{id}', [PaymentController::class, 'destroy']);
